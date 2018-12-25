18f28676644b3e54a3fad4c087ad7d7469eb4069
Yu Gai <yg1246@nyu.edu>
2018-12-25 17:17:56 +0800

    sync

--- reddit.py
+++ reddit.py
@@ -1,23 +1,19 @@
+import operator
 import sys
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 
-def row2kv_pair(row):
-    d = row.asDict()
-    return (d['parent_id'], d['id'])
-
-ss = SparkSession.builder.master("local").appName("reddit").getOrCreate()
+ss = SparkSession.builder.master('local').appName('reddit').config('spark.ui.showConsoleProgress', True).getOrCreate()
 sc = ss.sparkContext
 logger = sc._jvm.org.apache.log4j
-logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
-logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
+logger.LogManager.getLogger("org"). setLevel(logger.Level.WARN)
+logger.LogManager.getLogger("akka").setLevel(logger.Level.WARN)
 
 df = ss.read.format('json').load(sys.argv[1])
-rdd = sc.parallelize(df[['id', 'parent_id']].collect())
-kv_pairs = rdd.map(row2kv_pair)
-csr = kv_pairs.groupByKey().collect()
-print(type(csr))
-print(len(csr))
-print(csr[0])
-print(list(csr[0][1]))
+rdd = df[['id', 'parent_id']].rdd
+rdd = rdd.map(lambda row: (row['id'], row['parent_id']))
+rdd = rdd.groupWith(rdd)
+rdd = rdd.flatMap(lambda kvw: [((v, w), 1) for v in kvw[1][0] for w in kvw[1][1]])
+rdd.reduceByKey(operator.add)
+print(rdd.count())
