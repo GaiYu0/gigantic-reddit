3feaf0fe5126e83fa5684ad522539500583acc19
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-04-14 05:32:24 +0000

    commit

--- csr.py
+++ csr.py
@@ -5,13 +5,23 @@ sau = np.load('sau.npy')
 sav = np.load('sav.npy')
 m = len(sau)
 n = max(np.max(sau), np.max(sav)) + 1
+
+print(sau.min(), sau.max())
+print(sav.min(), sav.max())
+
 a2s = sps.coo_matrix((np.ones(m), (sau, sav)), shape=(n, n)).tocsr()
+a2s_indptr = a2s.indptr[sau.min():]
+a2s_indices = a2s.indices
+print('len(a2s.indptr)', len(a2s_indptr))
+print('len(a2s.indices)', len(a2s_indices))
+np.savetxt('a2s-indptr', a2s_indptr, '%d')
+np.savetxt('a2s-indices', a2s_indices, '%d')
+
 s2a = sps.coo_matrix((np.ones(m), (sav, sau)), shape=(n, n)).tocsr()
-print('len(a2s.indptr)', len(a2s.indptr))
-print('len(a2s.indices)', len(a2s.indices))
-np.savetxt('a2s-indptr', a2s.indptr, '%d')
-np.savetxt('a2s-indices', a2s.indices, '%d')
-print('len(s2a.indptr)', len(s2a.indptr))
-print('len(s2a.indices)', len(s2a.indices))
-np.savetxt('s2a-indptr', s2a.indptr, '%d')
-np.savetxt('s2a-indices', s2a.indices, '%d')
+s2a_indptr = s2a.indptr[:sau.min() + 1]
+s2a_indices = s2a.indices - sau.min()
+assert np.all(s2a_indptr[:-1] <= s2a_indptr[1:])
+print('len(s2a.indptr)', len(s2a_indptr))
+print('len(s2a.indices)', len(s2a_indices))
+np.savetxt('s2a-indptr', s2a_indptr, '%d')
+np.savetxt('s2a-indices', s2a.indices - sau.min(), '%d')
--- rw.c
+++ rw.c
@@ -1,74 +1,100 @@
+#include <assert.h>
 #include <math.h>
-#include <stdio.h>
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <unordered_set>
 #include <vector>
 
+#include <boost/unordered_set.hpp>
+#include <boost/functional/hash.hpp>
 #include <omp.h>
 
-const uint64_t n_authors = 1000000;
-const uint64_t n_comments = 1000000;
-const uint64_t n_submissions = 1000000;
-const uint64_t n_samples = 1000000;
+typedef std::pair<uint64_t, uint64_t> edge_t;
 
-uint64_t a2s_indptr[n_authors];
-uint64_t a2s_indices[n_comments];
-uint64_t s2a_indptr[n_authors];
-uint64_t s2a_indices[n_comments];
-std::vector<std::unordered_set<uint64_t>> author_sets(n_submissions);
-std::vector<float> norms(n_submissions);
+const uint64_t n_authors = 4240972 - 1;
+const uint64_t n_comments = 29753437;
+const uint64_t n_submissions = 27166161 - 1;
+const uint64_t n_samples = 1e9;
+const float threshold = 0.001;
+
+std::vector<uint64_t> a2s_indptr(n_authors + 1);
+std::vector<uint64_t> a2s_indices(n_comments);
+std::vector<uint64_t> s2a_indptr(n_submissions + 1);
+std::vector<uint64_t> s2a_indices(n_comments);
+std::vector<std::unordered_set<uint64_t>> a_sets(n_submissions);
+std::vector<float> normalizers(n_submissions);
 std::vector<uint64_t> uu(n_samples);
 std::vector<uint64_t> vv(n_samples);
 std::vector<bool> accepted(n_samples);
 
-void loadtxt(const char *f, uint64_t len, uint64_t *a) {
-    FILE *p = fopen(f, "r");
+void savetxt(const char *f, uint64_t len, uint64_t *a) {
+    FILE *p = fopen(f, "w");
     for (uint64_t i = 0; i < len; ++i) {
-        fscanf(p, "%lu", a + i);
+        fprintf(p, "%lu\n", a[i]);
+    }
+}
+
+void loadtxt(const char *f, std::vector<uint64_t> &a) {
+    std::fstream s(f, s.in);
+    for (uint64_t i = 0; i < a.size(); ++i) {
+        s >> a[i];
     }
 }
 
 int main() {
-    loadtxt("a2s_indptr", n_authors, a2s_indptr);
-    loadtxt("a2s_indices", n_authors, a2s_indices);
-    loadtxt("s2a_indptr", n_authors, s2a_indptr);
-    loadtxt("s2a_indices", n_authors, s2a_indices);
-
     #pragma omp parallel for
-    for (uint64_t i = 0; i < n_submissions; ++i) {
-        auto p = s2a_indices + s2a_indptr[i];
-        auto q = s2a_indices + s2a_indptr[i + 1];
-        author_sets[i] = p < q ? std::unordered_set<uint64_t>(p, q) : std::unordered_set<uint64_t>();
+    for (int i = 0; i < 4; ++i) {
+        switch (i) {
+        case 0:
+            loadtxt("a2s-indptr", a2s_indptr);
+            break;
+        case 1:
+            loadtxt("a2s-indices", a2s_indices);
+            break;
+        case 2:
+            loadtxt("s2a-indptr", s2a_indptr);
+            break;
+        case 3:
+            loadtxt("s2a-indices", s2a_indices);
+            break;
+        }
     }
 
+    std::cout << __LINE__ << std::endl;
     #pragma omp parallel for
     for (uint64_t i = 0; i < n_submissions; ++i) {
-        norms[i] = 1 / sqrt(static_cast<float>(author_sets[i].size()));
+        auto p = s2a_indices.begin() + s2a_indptr[i];
+        auto q = s2a_indices.begin() + s2a_indptr[i + 1];
+        a_sets[i] = p < q ? std::unordered_set<uint64_t>(p, q) : std::unordered_set<uint64_t>();
     }
 
-    std::vector<uint64_t> range(n_samples);
-    std::iota(range.begin(), range.end(), 0);
-    std::random_shuffle(range.begin(), range.end());
+    std::cout << __LINE__ << std::endl;
+    #pragma omp parallel for
+    for (uint64_t i = 0; i < n_submissions; ++i) {
+        normalizers[i] = 1 / sqrt(static_cast<float>(a_sets[i].size()));
+    }
+
+    std::cout << *std::max_element(normalizers.begin(), normalizers.end()) << ' '
+              << *std::min_element(normalizers.begin(), normalizers.end()) << std::endl;
+
+    std::cout << __LINE__ << std::endl;
+    #pragma omp parallel for
     for (uint64_t i = 0; i < n_samples; ++i) {
-        auto u = range[i];
         std::random_device rd;
-        auto engine = std::mt19937(rd);
-        std::uniform_int_distribution<> author_distribution(s2a_indptr[u], s2a_indptr[u + 1]);
-        auto a = s2a_indices[author_distribution(engine)];
-        std::uniform_int_distribution<> submission_distribution(s2a_indptr[a], s2a_indptr[a + 1]);
-        auto v = a2s_indices[submission_distribution(engine)];
-        uu[i] = u;
-        vv[i] = v;
-    }
+        std::uniform_int_distribution<> u_distr(0, n_submissions - 1);
+        auto u = u_distr(rd);
+        std::uniform_int_distribution<> a_distr(s2a_indptr.at(u), s2a_indptr.at(u + 1) - 1);
+        auto a = s2a_indices.at(a_distr(rd));
+        std::uniform_int_distribution<> v_distr(a2s_indptr.at(a), a2s_indptr.at(a + 1) - 1);
+        auto v = a2s_indices.at(v_distr(rd));
+        uu.at(i) = u;
+        vv.at(i) = v;
 
-    for (uint64_t i = 0; i < n_samples; ++i) {
-        auto u = uu[i];
-        auto v = vv[i];
         uint64_t less, more;
-        if (author_sets[u].size() < author_sets[v].size()) {
+        if (a_sets.at(u).size() < a_sets.at(v).size()) {
             less = u;
             more = v;
         } else {
@@ -76,6 +102,44 @@ int main() {
             more = u;
         }
         uint64_t n = 0;
+        for (const auto &a : a_sets.at(less)) {
+            n += a_sets.at(more).count(a);
+        }
+        float cos = n * normalizers.at(u) * normalizers.at(v);
+        accepted.at(i) = (cos >= threshold);
+    }
+
+    std::cout << __LINE__ << std::endl;
+    boost::unordered_set<edge_t> edges;
+    for (uint64_t i = 0; i < n_samples; ++i) {
+        if (accepted[i]) {
+            edges.insert(std::make_pair(uu[i], vv[i]));
+        }
+    }
+
+    std::cout << __LINE__ << std::endl;
+    std::vector<uint64_t> src;
+    std::vector<uint64_t> dst;
+    src.reserve(edges.size());
+    dst.reserve(edges.size());
+    for (const auto &uv : edges) {
+        src.push_back(uv.first);
+        dst.push_back(uv.second);
+    }
+
+    std::cout << "src.size()" << ' ' << src.size() << std::endl;
+    std::cout << "dst.size()" << ' ' << dst.size() << std::endl;
+
+    #pragma omp parallel for
+    for (int i = 0; i < 2; ++i) {
+        switch (i) {
+        case 0:
+            savetxt("src.sparsified", src.size(), src.data());
+            break;
+        case 1:
+            savetxt("dst.sparsified", dst.size(), dst.data());
+            break;
+        }
     }
 
     return 0;
