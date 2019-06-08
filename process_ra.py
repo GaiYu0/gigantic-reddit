ae0e949bdbea4b4cde8cb56da2daebcd5049a976
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-06-08 00:12:55 +0000

    commit

--- process_ra.py
+++ process_ra.py
@@ -1,4 +1,7 @@
 import argparse
+import dask.bag as db
+from dask.distributed import Client, progress
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--bs', type=int)
@@ -9,10 +12,14 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     user_bag = db.read_text(args.ra, blocksize=args.bs).map(json.loads)
-    user_df = user_bag.map(lambda d: {'uid' : int(d['id'].replace('t2_', ''), 36),
-                                      'username' : d['name']}).to_dataframe()
-    user_df = user_df.drop_duplicates(subset=['username'], keep=False).persist()
+    user_bag = user_bag.map(lambda d: {'uid' : int(d['id'].replace('t2_', ''), 36),
+                                       'username' : d['name']})
+    user_bag = user_bag.groupby(lambda d: d['username'])
+    user_bag = user_bag.map(lambda kv: [] if len(kv[1]) > 1 else kv[1])
+    user_df = user_bag.flatten().to_dataframe()
     user_df_indexed_by_uid = user_df.set_index(user_df.uid)
     user_df_indexed_by_username = user_df.set_index(user_df.username)
-    user_df_indexed_by_uid.to_hdf('user_df_indexed_by_uid-*', '/df', mode='w')
-    user_df_indexed_by_username.to_hdf('user_df_indexed_by_username-*', '/df', mode='w')
+    progress(user_df_indexed_by_uid)
+    progress(user_df_indexed_by_username)
+    user_df_indexed_by_uid.to_hdf('user_df/indexed_by_uid-*', '/df', mode='w')
+    user_df_indexed_by_username.to_hdf('user_df/indexed_by_username-*', '/df', mode='w')
