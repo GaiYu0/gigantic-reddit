de9422adcef1cd7a2388ea45b7484ad5a713e459
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-06-13 12:03:37 +0000

    commit

--- pyspark_proc_ra.py
+++ pyspark_proc_ra.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 from pyspark.sql.functions import regexp_replace
 from pyspark.sql.session import SparkSession
@@ -12,7 +13,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ss = SparkSession.builder.getOrCreate()
-    sc = ss.sparkContext
     user_df = ss.read.json(args.ra) \
                      .withColumnRenamed('id', 'uid') \
                      .withColumn('uid', regexp_replace('uid', 't2_', '')) \
@@ -20,5 +20,4 @@ if __name__ == '__main__':
                      .withColumnRenamed('name', 'username') \
                      .select('uid', 'username')
     u_df = user_df.groupBy('username').count().filter('count = 1').drop('count')
-    user_df.join(u_df, on='username').toPandas().to_hdf('user-df', 'df')
-#   user_df.join(u_df, on='username').write.orc('user-df', mode='overwrite')
+    user_df.join(u_df, ['username']).write.orc('user-df', mode='overwrite')
