1d603110a06522aed6e1bc121a6cf52e5aff27b7
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-06-13 20:24:32 +0800

    commit

--- pyspark_time_window_state_0.py
+++ pyspark_time_window_state_0.py
@@ -1,4 +1,36 @@
-session = SparkSession.builder.getOrCreate()
-context = session.sparkContext
-logger = context._jvm.org.apache.log4j
+import argparse
+import multiprocessing as mp
 
+import numpy as np
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import IntegerType
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--rc', type=str, nargs='+')
+parser.add_argument('--rs', type=str, nargs='+')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    ss = SparkSession.builder.getOrCreate()
+    user_df = ss.read.orc('user-df')
+    cmnt_df = None
+    for f in args.rc:
+        df = ss.read.json(f)
+        cmnt_df = df if cmnt_df is None else cmnt_df.union(df)
+    cmnt_df = cmnt_df.withColumnRenamed('link_id', 'pid') \
+                     .withColumn('pid', regexp_replace('pid', 't3_', '')) \
+                     .withColumn('pid', utils.udf_int36('pid')) \
+                     .withColumnRenamed('author', 'username') \
+                     .withColumnRenamed('created_utc', 'utc') \
+                     .withColumn('utc', cmnt_df.utc.cast(IntegerType)) \
+                     .join(user_df, ['username'])
+
+    pid = np.array(cmnt_df.select("pid").rdd.flatMap(lambda x: x).collect())
+    uid = np.array(cmnt_df.select("uid").rdd.flatMap(lambda x: x).collect())
+    utc = np.array(cmnt_df.select("utc").rdd.flatMap(lambda x: x).collect())
+
+    with mp.Pool(3) as pool:
+        pool.map(lambda key : {'pid' : np.savetxt('pid', pid, '%d'),
+                               'uid' : np.savetxt('uid', uid, '%d'),
+                               'utc' : np.savetxt('utc', utc, '%d')}[key], ['pid', 'uid', 'utc'])
