72a15faf5b2f13fb86f9e76a92d40ed3d9d85576
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-06-14 23:09:26 +0000

    commit

--- pyspark_proc_post.py
+++ pyspark_proc_post.py
@@ -15,14 +15,11 @@ ss = SparkSession.builder.getOrCreate()
 user_df = ss.read.orc('user-df')
 post_df = None
 for f in args.rs:
-    df = ss.read.json(f).select('id', 'author', 'subreddit_id', 'title')
+    df = ss.read.json(f).select('id', 'subreddit_id', 'title')
     post_df = df if post_df is None else post_df.union(df)
 post_df = post_df.withColumn('pid', utils.udf_int36('id')) \
-                 .withColumnRenamed('author', 'username') \
                  .dropna(subset=['subreddit_id']) \
                  .withColumn('srid', utils.udf_int36(regexp_replace('subreddit_id', 't5_', '')))
 srid = map(int, np.load('srid.npy'))
 post_df.filter(post_df.srid.isin(*srid)) \
-       .join(user_df, ['username']) \
-       .drop('username') \
        .write.orc('post-df', mode='overwrite')
