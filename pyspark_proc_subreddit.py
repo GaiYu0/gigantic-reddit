701974d20f05b8d6540332ea3252a3ae1b2efb03
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-06-14 12:51:53 +0000

    commit

--- pyspark_proc_subreddit.py
+++ pyspark_proc_subreddit.py
@@ -8,21 +8,14 @@ import utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--rx', type=str, nargs='+')
-parser.add_argument('--hi', type=str)
-parser.add_argument('--lo', type=str)
 args = parser.parse_args()
 
-lo = eval(args.lo)
-hi = eval(args.hi)
 ss = SparkSession.builder.getOrCreate()
 df = None
 for f in args.rx:
     df = ss.read.json(f)
     df = df if df is None else df.union(df)
-subreddits = next(zip(*sorted(df.groupBy('srid').count().rdd.map(lambda x: [x['srid'], x['count']]).collect(), key=utils.snd, reverse=True)[lo : hi]))
 df = df.dropna(subset=['subreddit_id']) \
        .withColumn('srid', utils.udf_int36(regexp_replace('subreddit_id', 't5_', ''))) \
        .groupBy('srid').count().toPandas()
-srid_df = df.sort_values(by='count', ascending=False)[lo : hi]
-srid = list(srid_df['srid'])
-pickle.dump(srid, 'srid')
+df.sort_values(by='count', ascending=False).to_hdf('srid-df', '/df')
