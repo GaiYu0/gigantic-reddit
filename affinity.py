d87a6281707cc2d5a49b6b3140354a515c403d9a
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-06-21 05:12:30 +0000

    commit

--- affinity.py
+++ affinity.py
@@ -5,8 +5,9 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('--src', type=str, default='src.npy')
 parser.add_argument('--dst', type=str, default='dst.npy')
-parser.add_argument('--y', type=str, default='y.npy')
 parser.add_argument('--n-classes', type=int)
+parser.add_argument('-p', action='store_true')
+parser.add_argument('--y', type=str, default='y.npy')
 args = parser.parse_args()
 
 src = np.load(args.src)
@@ -16,7 +17,6 @@ y = np.load(args.y)
 y_src = y[src]
 y_dst = y[dst]
 ns = dask.compute(*[dask.delayed(lambda i: np.sum(y == i))(i) for i in range(args.n_classes)])
-print(ns)
-a = dask.compute([[dask.delayed(lambda i, j, m, n: np.sum((y_src == i) & (y_dst == j)) / (m * n))(i, j, m, n) for j, n in enumerate(ns)] for i, m in enumerate(ns)])
+a = dask.compute([[dask.delayed(lambda i, j, m, n: np.sum((y_src == i) & (y_dst == j)) / (m * n if args.p else 1))(i, j, m, n) for j, n in enumerate(ns)] for i, m in enumerate(ns)])
 # np.set_printoptions(precision=3)
 print(np.array(a))
