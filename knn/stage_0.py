bc2632d1042b98ef6495ac4dcbe13e20142cf739
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-06-22 19:18:37 +0800

    commit

--- knn/stage_0.py
+++ knn/stage_0.py
@@ -5,21 +5,19 @@ from pyspark.sql.session import SparkSession
 ss = SparkSession.builder.getOrCreate()
 sc = ss.sparkContext
 
-# user_df = ss.read.orc('user-df')
-# post_df = ss.read.orc('post-df')
 cmnt_df = ss.read.orc('cmnt-df')
 user_df = cmnt_df.select('uid').dropDuplicates()
-post_df = cmnt_df.select('pid').dropDuplicates()
+post_df = ss.read.orc('post-df').join(cmnt_df.select('pid'), 'pid').dropDuplicates()
 
 collect_column = lambda df, column: df.select(column).rdd.flatMap(lambda x: x).collect()
 cmnt_df = cmnt_df.join(sc.parallelize(zip(collect_column(post_df, 'pid'), range(post_df.count()))).toDF(['pid', 'compact_pid']), 'pid') \
                  .join(sc.parallelize(zip(collect_column(user_df, 'uid'), range(user_df.count()))).toDF(['uid', 'compact_uid']), 'uid')
 
-p2u = cmnt_df.rdd.map(lambda row: [row['compact_pid'], row['compact_uid']]).groupByKey().sortByKey()
+p2u = cmnt_df.rdd.map(lambda row: [row.compact_pid, row.compact_uid]).groupByKey().sortByKey()
 p2u_indptr = np.cumsum([0] + p2u.map(lambda kv: len(kv[1])).collect())
 p2u_indices = np.array(p2u.flatMap(lambda kv: kv[1]).collect())
 
-u2p = cmnt_df.rdd.map(lambda row: [row['compact_uid'], row['compact_pid']]).groupByKey().sortByKey()
+u2p = cmnt_df.rdd.map(lambda row: [row.compact_uid, row.compact_pid]).groupByKey().sortByKey()
 u2p_indptr = np.cumsum([0] + u2p.map(lambda kv: len(kv[1])).collect())
 u2p_indices = np.array(u2p.flatMap(lambda kv: sorted(kv[1])).collect())
 
@@ -30,3 +28,8 @@ dask.compute(dask.delayed(np.savetxt)('p2u-indptr', p2u_indptr, fmt='%d'),
              dask.delayed(np.savetxt)('p2u-indices', p2u_indices, fmt='%d'),
              dask.delayed(np.savetxt)('u2p-indptr', u2p_indptr, fmt='%d'),
              dask.delayed(np.savetxt)('u2p-indices', u2p_indices, fmt='%d'))
+
+y = np.array(post_df.join(cmnt_df.select('pid', 'compact_pid').dropDuplicates(), 'pid').rdd.map(lambda row: [row.compact_pid, row.srid]).sortByKey().map(lambda kv: kv[1]).collect())
+unique, inverse = np.unique(y, return_inverse=True)
+y = np.arange(len(unique))[inverse]
+np.save('y', y)
---
+++ knn/stage_2.py
@@ -0,0 +1,17 @@
+import numpy as np
+from pyspark.sql.session import SparkSession
+import scipy.sparse as sps
+
+import utils
+
+ss = SparkSession.builder.getOrCreate()
+sc = ss.sparkContext
+
+src = utils.loadtxt(sc, 'src', int, np.int64)
+dst = utils.loadtxt(sc, 'dst', int, np.int64)
+n = np.max(src) + 1
+a = sps.coo_matrix((np.ones_like(src), (src, dst)), [n, n])
+b = a.maximum(a.transpose())
+
+np.save('src', b.row)
+np.save('dst', b.col)
