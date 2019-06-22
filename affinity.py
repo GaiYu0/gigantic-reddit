244c8416a092fb308f07daa43a043bdf2c5ab0ea
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-06-23 06:40:21 +0800

    commit

--- affinity.py
+++ affinity.py
@@ -18,5 +18,5 @@ y_src = y[src]
 y_dst = y[dst]
 ns = dask.compute(*[dask.delayed(lambda i: np.sum(y == i))(i) for i in range(args.n_classes)])
 a = dask.compute([[dask.delayed(lambda i, j, m, n: np.sum((y_src == i) & (y_dst == j)) / (m * n if args.p else 1))(i, j, m, n) for j, n in enumerate(ns)] for i, m in enumerate(ns)])
-# np.set_printoptions(precision=3)
+np.set_printoptions(precision=3)
 print(np.array(a))
