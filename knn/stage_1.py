ac54922341a5c5cff9b0038051718a496ab72076
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-06-22 22:00:55 +0800

    commit

--- knn/stage_1.py
+++ knn/stage_1.py
@@ -16,3 +16,13 @@ n = len(u2p_indptr) - 1
 p2u = sps.csr_matrix((np.ones_like(p2u_indices), p2u_indices, p2u_indptr), shape=(m, n))
 u2p = sps.csr_matrix((np.ones_like(u2p_indices), u2p_indices, u2p_indptr), shape=(n, m))
 p2p = p2u @ u2p
+
+idx = p2p.indptr[1 : -1]
+rdd = ss.parallelize(zip(np.split(p2p.indices, idx), np.split(p2p.data, idx)))
+k = 5
+rdd = rdd.map(lambda indices, data: indices[np.argsort(data)[-k:]])
+indptr = np.array(rdd.map(len).collect())
+indices = np.array(rdd.flatMap(lambda x: x).collect())
+a = sps.csr_matrix((np.ones_like(indices), indices, indptr), shape=(m, m))
+b = a.maximum(a.transpose())
+print('end')
