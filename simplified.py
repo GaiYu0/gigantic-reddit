4475dc1b0e5ddd78c801b41cc7a406b429d0d330
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-08-10 10:03:20 +0800

    commit

--- simplified.py
+++ simplified.py
@@ -12,6 +12,7 @@ int36 = udf(partial(int, base=36), IntegerType())
 parser = argparse.ArgumentParser()
 parser.add_argument('--rc', type=str, nargs='+')
 parser.add_argument('--rs', type=str, nargs='+')
+parser.add_argument('--subreddits', type=str, nargs='+')
 args = parser.parse_args()
 
 ss = SparkSession.builder.getOrCreate()
@@ -19,22 +20,29 @@ sc = ss.sparkContext
 
 rs = None
 for x in args.rs:
-    df = ss.read.json(x).select('id').dropna('any')
+    df = ss.read.json(x).select('id', 'subreddit_id')
     rs = df if rs is None else rs.union(df)
+rs = rs.dropna('any')
 
 rc = None
 for x in args.rc:
-    df = ss.read.json(x).select('author', 'created_utc', 'link_id').dropna('any')
+    df = ss.read.json(x).select('author', 'created_utc', 'link_id')
     rc = df if rc is None else rc.union(df)
+rc = rc.dropna('any')
 
-rc = rc.withColumnRenamed('link_id', 'pid')
-rc = rc.withColumn('pid', regexp_replace(rc.pid, 't3_', ''))
-rc = rc.withColumn('pid', int36(rc.pid))
+rs = rs.withColumnRenamed('subreddit_id', 'srid')
+rs = rs.filter(rs.srid in pickle.load(open(args.subreddits, 'rb')))
+rs = rs.withColumn('srid', regexp_replace(rs.srid, 't5_', ''))
+rs = rs.withColumn('srid', int36(rs.srid))
 
-rs = rs.withColumnRenamed('id', 'pid')
-rs = rs.withColumn('pid', int36(rs.pid))
+rc = rc.withColumnRenamed('link_id', 'link_id')
+rc = rc.withColumn('link_id', regexp_replace(rc.link_id, 't3_', ''))
+rc = rc.withColumn('link_id', int36(rc.link_id))
 
-rc = rc.join(rs, 'pid')
+rs = rs.withColumn('id', int36(rs.id))
+link_ids = rs.select('id').rdd.flatMap(id_map).collect()
+df = sc.parallelize(zip(range(len(link_ids)), link_ids)).toDF('pid INT, link_id INT')
+rc = rc.join(df, 'link_id')
 
 authors = rc.select('author').dropDuplicates().rdd.flatMap(id_map).collect()
 authors.remove('[deleted]')
@@ -47,9 +55,7 @@ pid = np.array(rc.select('pid').rdd.flatMap(id_map).collect())
 uid = np.array(rc.select('uid').rdd.flatMap(id_map).collect())
 utc = np.array(rc.select('utc').rdd.flatMap(id_map).collect())
 
-uniq, inv = np.unique(pid, return_inverse=True)
-pid = np.arange(len(uniq))[inv]
-
 np.save('pid', pid)
 np.save('uid', uid)
 np.save('utc', utc)
+
